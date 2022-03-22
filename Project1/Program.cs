6684d6754d17f36d40a051000d2c9dc40d87e13f
Damiaan <ddamian2057@gmail.com>
2022-03-22 09:50:51 +0100

    Additional tests and the method of adding two values

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace FirstProgram
 {
     public class Program
     {
@@ -8,9 +8,24 @@ namespace ConsoleApp1
         {
             return "Test";
         }
+
+        public static double add(double x, double y)
+        {
+            return x + y;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Dodaje dwie wartosci!");
+
+            Console.WriteLine("Podaj pierwsza liczbe:");
+            double x = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj druga liczbe:");
+            double y = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wynik to:"+add(x,y));
         }
     }
 }
