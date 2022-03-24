50128a10c8ad7a1e0f78e6ab5d027305657100b8
236514 Dangreaux Mateusz <236514@edu.p.lodz.pl>
2022-03-24 17:49:14 +0100

    Simplification of code, toTestMethod returns "Hello world"

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,26 +6,12 @@ namespace FirstProgram
     {
         public static string toTestMethod()
         {
-            return "Test";
-        }
-
-        public static double add(double x, double y)
-        {
-            return x + y;
+            return "Hello World";
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Dodaje dwie wartosci!");
-
-            Console.WriteLine("Podaj pierwsza liczbe:");
-            double x = Double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Podaj druga liczbe:");
-            double y = Double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Wynik to:"+add(x,y));
+            Console.Write(toTestMethod());
         }
     }
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -8,16 +8,8 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("Test", FirstProgram.Program.toTestMethod());
-        }
-
-        [TestMethod]
-        public void TestMethod2()
-        {
-            Assert.AreEqual(8, FirstProgram.Program.add(5,3));
-            Assert.AreNotEqual(8, FirstProgram.Program.add(5, 4));
-            Assert.AreEqual(8, FirstProgram.Program.add(5.5, 2.5));
-            Assert.AreEqual(-8, FirstProgram.Program.add(-9.5, 1.5));
+            Assert.AreEqual(FirstProgram.Program.toTestMethod(), "Hello World");
+            Assert.AreNotEqual(FirstProgram.Program.toTestMethod(), "Goodbye World");
         }
     }
 }
