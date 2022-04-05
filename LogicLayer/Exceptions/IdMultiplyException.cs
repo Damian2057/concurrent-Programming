75139adc195b844dc0992c059aa4f7c25167ee8c
Damiaan <ddamian2057@gmail.com>
2022-04-05 21:11:22 +0200

    Adding own exceptions

---
+++ LogicLayer/Exceptions/IdMultiplyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.Exceptions
+{
+    [Serializable]
+    public class IdMultiplyException : Exception
+    {
+        public IdMultiplyException() : base() { }
+        public IdMultiplyException(string message) : base(message) { }
+        public IdMultiplyException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ LogicLayer/Exceptions/InvalidDataException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.Exceptions
+{
+    [Serializable]
+    public class InvalidDataException : Exception
+    {
+        public InvalidDataException() : base() { }
+        public InvalidDataException(string message) : base(message) { }
+        public InvalidDataException(string message, Exception inner) : base(message, inner) { }
+    }
+}
