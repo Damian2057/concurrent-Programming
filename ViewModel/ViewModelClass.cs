6ab8c682b22e5ba5de1b52bc089b9220533ff31e
236514 Dangreaux Mateusz <236514@edu.p.lodz.pl>
2022-04-14 16:19:23 +0200

    Start of ViewModel implementation

--- ViewModel/ViewModelClass.cs
+++ ViewModel/ViewModelClass.cs
@@ -1,9 +1,47 @@
 ﻿using Presentation.ViewModel.MVVMcore;
+using Presentation.Model;
 
 namespace Presentation.ViewModel
 {
     public class ViewModelClass : BaseViewModel
     {
-       
+        private string _numberOfBalls;
+        public RelayCommand _start { get; }
+        public RelayCommand _stop { get; }
+
+        public bool _flag = true;
+
+        public MainMap _mainMap{ get; }
+
+        public ViewModelClass()
+        {
+            _start = new RelayCommand(); //to do
+            _stop = new RelayCommand(); //to do
+            _numberOfBalls = "";
+            _mainMap = new MainMap();
+        }
+
+        public string NumberOfBalls
+        {
+            get => _numberOfBalls;
+            set
+            {
+                _numberOfBalls = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Button
+        {
+            get => _flag;
+
+            set
+            {
+                _flag = value;
+                OnPropertyChanged();
+            }
+        }
+
+
     }
 }
