dd0be54a2e808de2ff3d86f52d209f404a9268d1
Damiaan <ddamian2057@gmail.com>
2022-05-11 19:55:37 +0200

    Preparation for Stage 2

--- Tests/DataTest/DataTest.cs
+++ Tests/DataTest/DataTest.cs
@@ -1,76 +1,14 @@
-﻿using Data;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests.DataTest
 {
-    [TestClass]
     public class DataTest
     {
         [TestMethod]
-        public void BallConstructorTest()
+        public void Test1()
         {
 
-            var ball = BallApi.CreateBall(0,0, 1,0, 2, 3);
-            Assert.AreEqual(0, ball.XPos);
-            Assert.AreEqual(1, ball.YPos);
-            Assert.AreEqual(2, ball.XDirection);
-            Assert.AreEqual(3, ball.YDirection);
-            Assert.AreEqual(0, ball.BallID);
-            Assert.AreEqual(0, ball.Radius);
-
-            Assert.IsTrue(ball.XPos >= 0);
-            Assert.IsTrue(ball.YPos >= 0);
-            Assert.IsTrue(ball.XDirection >= 0);
-            Assert.IsTrue(ball.YDirection >= 0);
-            Assert.IsTrue(ball.BallID >= 0);
-            Assert.IsTrue(ball.Radius >= 0);
-            Assert.IsNotNull(ball.color);
         }
 
-        [TestMethod]
-        public void ObjectStorageTest()
-        {
-
-            var objectStorage = BallRepositoryApi.CreateRepository();
-
-            var ball = BallApi.CreateBall(1,0, 1,0, 2, 3);
-            objectStorage.AddBall(ball);
-
-            Assert.AreEqual(objectStorage.GetAllBalls().Count, 1);
-            var ball2 = BallApi.CreateBall(2,0, 1,0, 2, 3);
-            objectStorage.AddBall(ball2);
-
-            Assert.AreEqual(objectStorage.GetAllBalls().Count, 2);
-
-            objectStorage.RemoveBall(ball);
-
-            Assert.AreEqual(objectStorage.GetAllBalls().Count, 1);
-
-            objectStorage.ClearStorage();
-
-            Assert.AreEqual(objectStorage.GetAllBalls().Count, 0);
-        }
-
-        [TestMethod]
-        public void ColorTest()
-        {
-            //test for a color selected from a premade array
-            var ball = BallApi.CreateBall(1, 0, 1, 0, 2, 3); 
-            Assert.IsTrue(ball.color != "");
-            Assert.IsTrue(ball.color is not null);
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-            Assert.IsTrue(ball.color.Length == 7);
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            Assert.IsTrue(ball.color.StartsWith("#"));
-
-            //test for a pseudorandom color
-            var ball2 = BallApi.CreateBall(1337, 5, 5, 5, 5, 5);
-            Assert.IsTrue(ball2.color != "");
-            Assert.IsTrue(ball2.color is not null);
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-            Assert.IsTrue(ball2.color.Length == 7);
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            Assert.IsTrue(ball2.color.StartsWith("#"));
-        }
     }
 }
--- Tests/LogicLayerTest/LogicTest.cs
+++ Tests/LogicLayerTest/LogicTest.cs
@@ -1,86 +1,12 @@
-using System.Linq;
-using LogicLayer;
-using LogicLayer.Exceptions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Tests.LogicTest
+namespace Tests.LogicLayerTest
 {
-    [TestClass]
     public class LogicTest
     {
         [TestMethod]
-        public void BallServiceTest()
+        public void Test1()
         {
-            var service = BallServiceApi.CreateLogic(150, 100);
-            service.SummonBalls(10);
-            Assert.AreEqual(service.GetAllBalls().Count, 10);
-            Assert.AreEqual(service.GetBallByID(1).BallID, 1);
-
-            service.RemoveBallByID(1);
-            Assert.AreEqual(service.GetAllBalls().Count, 9);
-            Assert.ThrowsException<InvalidDataException>(() => service.GetBallByID(1));
-
-            int x1 = service.GetBallByID(2).XPos;
-            int y1 = service.GetBallByID(2).YPos;
-            service.DoTick();
-            Assert.IsTrue(service.GetBallByID(2).XPos != x1 || service.GetBallByID(2).YPos != y1);
-
-            service.ClearMap();
-            Assert.AreEqual(service.GetAllBalls().Count, 0);
-            Assert.ThrowsException<InvalidDataException>(() => service.GetBallByID(5));
-        }
-
-        [TestMethod]
-        public void TickTest()
-        {
-            var ballsManager = BallServiceApi.CreateLogic(150, 100);
-            //XY CORDS
-            ballsManager.SummonBalls(2);
-            int xCurrentPos = ballsManager.GetBallByID(1).XPos;
-            int xHeading = ballsManager.GetBallByID(1).XDirection;
-
-            int xPredictedPos = 0;
-            int xPredictedHeading = 0;
-
-            if (ballsManager.GetBallByID(1).XPos
-                + ballsManager.GetBallByID(1).XDirection
-                + ballsManager.GetBallByID(1).Radius > ballsManager.GetMapWidth() || ballsManager.GetBallByID(1).XPos
-                + ballsManager.GetBallByID(1).XDirection
-                + ballsManager.GetBallByID(1).Radius < 2*ballsManager.GetBallByID(1).Radius) // tu chyba powinno byc 0
-            {
-                xPredictedHeading = xHeading * (-1);
-            }
-            else
-            {
-                xPredictedHeading = xHeading;
-            }
-            xPredictedPos = xCurrentPos + xPredictedHeading;
-            ballsManager.DoTick();
-            Assert.AreEqual(ballsManager.GetBallByID(1).XPos, xPredictedPos);
-
-            int yCurrentPos = ballsManager.GetBallByID(1).YPos;
-            int yHeading = ballsManager.GetBallByID(1).YDirection;
-
-            int yPredictedPos = 0;
-            int yPredictedHeading = 0;
-
-
-            if (ballsManager.GetBallByID(1).YPos
-                + ballsManager.GetBallByID(1).YDirection
-                + ballsManager.GetBallByID(1).Radius > ballsManager.GetMapHeight() || ballsManager.GetBallByID(1).YPos
-                + ballsManager.GetBallByID(1).YDirection
-                + ballsManager.GetBallByID(1).Radius < 2 * ballsManager.GetBallByID(1).Radius)
-            {
-                yPredictedHeading = yHeading * (-1);
-            }
-            else
-            {
-                yPredictedHeading = yHeading;
-            }
-            yPredictedPos = yCurrentPos + yPredictedHeading;
-            ballsManager.DoTick();
-            Assert.AreEqual(ballsManager.GetBallByID(1).YPos, yPredictedPos);
-
 
         }
     }
