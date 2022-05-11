43bdf506380334ed8b469f0b73c2871cf2655712
Mateusz Dangreaux <mateuszdangreaux@gmail.com>
2022-05-11 20:19:11 +0200

    Implementing Ball and BallApi

---
+++ Data/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class Ball: BallApi
+    {
+        private double _xPos;
+        private double _yPos;
+        private double _radius;
+        private double _mass;
+
+        public Ball(double xPos, double yPos, double radius, double mass)
+        {
+            _xPos = xPos;
+            _yPos = yPos;
+            _radius = radius;
+            _mass = mass;
+        }
+
+        public override double xPos { get; set; }
+        public override double yPos { get; set; }
+        public override double radius { get; set; }
+        public override double mass { get; set; }
+
+    }
+}
---
+++ Data/BallApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public abstract class BallApi
+    {
+        public static BallApi CreateBall(double xPos, double yPos, double radius, double mass)
+        {
+            return new Ball();
+        }
+
+        public abstract double xPos { get; set; }
+        public abstract double yPos { get; set; }
+        public abstract double radius { get; set; }
+        public abstract double mass { get; set; }
+
+
+    }
+}
