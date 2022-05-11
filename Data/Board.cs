810789612cd9c9e27bb996e30cc0599fd4e6770e
Mateusz Dangreaux <mateuszdangreaux@gmail.com>
2022-05-11 20:45:22 +0200

    Implementing board.cs

--- Data/Board.cs
+++ Data/Board.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Data
 {
-    internal class Board
+    public class Board
     {
+        private readonly int _width;
+        private readonly int _height;
+        private readonly List<BallApi> _balls = new();
+
+        public Board(int width, int height)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        public int Width { get { return _width; } }
+        public int Height { get { return _height; } }
+
+        public void AddBall(BallApi ball)
+        {
+            _balls.Add(ball);
+        }
+
+        public List<BallApi> GetBalls()
+        {
+            return _balls;
+        }
+
+        public void ClearBalls()
+        {
+            _balls.Clear();
+        }
     }
 }
