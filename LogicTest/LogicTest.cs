5e77b7dc8dc10f5563cec028c6d585f341ccce00
Mateusz Dangreaux <mateuszdangreaux@gmail.com>
2022-05-12 20:27:06 +0200

    Adding Data tests

--- LogicTest/LogicTest.cs
+++ LogicTest/LogicTest.cs
@@ -3,11 +3,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LogicTest
 {
     [TestClass]
-    public class UnitTest1
+    public class LogicTest
     {
         [TestMethod]
         public void TestMethod1()
         {
+
         }
     }
 }
