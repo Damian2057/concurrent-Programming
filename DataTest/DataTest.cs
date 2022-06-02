14e66abcc72b157836209b01c403b668048486e1
Mateusz Dangreaux <mateuszdangreaux@gmail.com>
2022-06-02 21:45:07 +0200

    Adding new testmethod in DataTest

--- DataTest/DataTest.cs
+++ DataTest/DataTest.cs
@@ -11,14 +11,22 @@ namespace DataTest
         private readonly Vector2 boardSize = new Vector2(800, 600);
 
         [TestMethod]
-        public void BoardTest()
+        public void BoardTest1()
         {
             balls = BallApi.CreateBallsList(boardSize);
             Assert.AreEqual(boardSize, balls.BoardHeightAndWidth);
 
         }
 
+        [TestMethod]
+        public void BoardTest2()
+        {
+            var ballsVar = BallApi.CreateBallsList(boardSize);
+            ballsVar.AddBalls(5);
+            Assert.IsNotNull(ballsVar);
+            Assert.AreEqual(boardSize, ballsVar.BoardHeightAndWidth);
+
+        }
+
     }
-
-
 }
